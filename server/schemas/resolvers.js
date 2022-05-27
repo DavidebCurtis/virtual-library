f6a2c3b280e488f8e5ac5d690d15d53416c51a5f
david curtis <heythereimdavidcurtis@gmail.com>
2022-05-26 20:14:56 -0400

    added resolver mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -49,7 +49,19 @@ const resolvers = {
         const updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id },
           { $addToSet: { savedBooks: body } },
-          { new: true, runValidators: true }
+          { new: true }
+        );
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    removeBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: args.bookId } } },
+          { new: true }
         );
         return updatedUser;
       }
